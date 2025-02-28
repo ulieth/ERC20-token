b9c5ff18ba75cc3297a3dc72b67d81d2f061a79a
ulieth <uliana.eth@proton.me>
2025-02-28 17:02:19 +0300

    adding oppenzeppelin for ERC20

---
+++ src/OurToken.sol
@@ -0,0 +1,11 @@
+// SPDX-Licence-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract OurToken is ERC20 {
+  constructor(uint256 initialSupply) ERC20("OurToken", "OT") {
+    _mint(msg.sender, initialSupply);
+  }
+}
